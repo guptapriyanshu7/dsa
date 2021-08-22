43d4bc7065a96c040a0b5ccf420f996ca5e0a735
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-08-22 22:43:01 +0530

    commit
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- leetcode/198. House Robber.cpp
+++ leetcode/198. House Robber.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
-int solve(vector<int>& nums) {
+int recurse(int i, vector<int>& nums, vector<int>& memo) {
+  if (i >= nums.size()) return 0;
+  if (memo[i] != -1) return memo[i];
+  memo[i] = max(recurse(i + 2, nums, memo) + nums[i], recurse(i + 1, nums, memo));
+  return memo[i];
+}
 
+int solve(vector<int>& nums) {
+  vector<int> memo(nums.size(), -1);
+  return recurse(0, nums, memo);
 }
 
 int main() {
-  vector<int> nums{ 1,2,3,1 };
-  solve(nums);
+  vector<int> nums{ 2,7,9,3,1 };
+  cout << solve(nums);
   return 0;
 }
