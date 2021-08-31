27f825e2319f2ea90f47aba54516e400049c6ab3
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-08-31 13:58:08 +0530

    commit
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- leetcode/35. Search Insert Position.cpp
+++ leetcode/35. Search Insert Position.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int solve(vector<int>& nums, int target) {
-  int left = 0, right = nums.size();
-  while (left < right) {
-    int mid = (left + right) / 2;
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
     if (nums[mid] == target)
       return mid;
     else if (nums[mid] < target)
-      left = mid;
-    else right = mid;
+      left = mid + 1;
+    else right = mid - 1;
   }
-  return 100;
+  return left;
 }
 
 int main() {
