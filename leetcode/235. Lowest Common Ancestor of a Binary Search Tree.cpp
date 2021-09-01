2e6bf141bef078d4b9c8a58b26c6fd22827253ba
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-09-01 23:10:08 +0530

    commit
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- leetcode/235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ leetcode/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -42,11 +42,10 @@ TreeNode* TreeFromArray(vector<int>& array) {
 }
 
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-  if (p->val < root->val && q->val < root->val) {
+  if (p->val < root->val && q->val < root->val)
     return lowestCommonAncestor(root->left, p, q);
-  } else if (p->val > root->val && q->val > root->val) {
+  else if (p->val > root->val && q->val > root->val)
     return lowestCommonAncestor(root->right, p, q);
-  }
   return root;
 }
 
