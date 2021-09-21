ed6e85438b51662759089da55f8738e3678b9ae4
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-09-21 20:58:52 +0530

    commit
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- add.cpp
+++ add.cpp
@@ -1,52 +1,57 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-class fi;
-class mc {
-   int m, cm;
-public:
-   mc() {
-      m = 0;
-      cm = 0;
-   }
-   void getdata() {
-      cout << "\nEnter the length in meter and centimeter ";
-      cin >> m >> cm;
-   }
-   friend fi add(fi, mc);
+
+class Fi;
+
+class Mc {
+  int m_m = 0, m_cm = 0;
+
+  public:
+  void getdata() {
+    cout << "\nEnter the length in meter and centimeter ";
+    cin >> m_m >> m_cm;
+  }
+
+  friend Fi add(Fi f1, Mc m1);
 };
-class fi {
-   int f, i;
-public:
-   void getdata() {
-      cout << "\nEnter the length in feet and inch ";
-      cin >> f >> i;
-   }
-   void putdata() {
-      cout << f << " feet" << i << " inch";
-   }
-   friend fi add(fi, mc);
+
+class Fi {
+  int m_f = 0, m_i = 0;
+
+  public:
+  void getdata() {
+    cout << "\nEnter the length in feet and inch ";
+    cin >> m_f >> m_i;
+  }
+
+  void putdata() const {
+    cout << m_f << " feet" << m_i << " inch";
+  }
+
+  friend Fi add(Fi f1, Mc m1);
 };
-fi add(fi f1, mc m1) {
-   fi t;
-   float j;
-   j = m1.m * 100 + m1.cm;
-   j = j * 0.393700787;
-   t.f = j / 12;
-   int k = int(j);
-   t.i = k % 12;
-   t.i = t.i + f1.i;
-   t.f = (t.i / 12) + t.f + f1.f;
-   t.i = t.i % 12;
-   return(t);
+
+Fi add(Fi f1, Mc m1) {
+  Fi t{};
+  float j = m1.m_m * 100 + m1.m_cm;
+  j       = j * 0.393700787;
+  t.m_f   = j / 12;
+  auto k  = static_cast<int>(j);
+  t.m_i   = k % 12;
+  t.m_i   = t.m_i + f1.m_i;
+  t.m_f   = (t.m_i / 12) + t.m_f + f1.m_f;
+  t.m_i   = t.m_i % 12;
+  return t;
 }
+
 int main() {
-   mc m1;
-   fi f1, f2;
-   m1.getdata();
-   f1.getdata();
-   f2 = add(f1, m1);
-   cout << "\n Sum length ";
-   f2.putdata();
-   return 0;
+  Mc m1;
+  Fi f1{}, f2{};
+  m1.getdata();
+  f1.getdata();
+  f2 = add(f1, m1);
+  cout << "\nSum length ";
+  f2.putdata();
+  return 0;
 }
