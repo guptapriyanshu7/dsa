7625f23f80a8e2e2ee48087554dd1d95ca4fab76
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-09-23 00:40:37 +0530

    commit
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- leetcode/45. Jump Game II.cpp
+++ leetcode/45. Jump Game II.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int recurse(int i, const vector<int>& nums, vector<int>& dp) {
   if (i >= nums.size() - 1) return 0;
   if (dp[i] != -1) return dp[i];
-  auto res = INT_MAX;
+  auto res = INT_MAX - 1;
   for (size_t j = 1; j <= nums[i]; j++) {
     res = min(res, 1 + recurse(i + j, nums, dp));
   }
