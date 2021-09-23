6b6d1ccb7fd252741bd5a5007789687f9b9165eb
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-09-23 21:41:20 +0530

    commit
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- memory-leaks.cpp
+++ memory-leaks.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+#include <memory>
+
 using namespace std;
 
 struct Memory {
@@ -7,9 +9,35 @@ struct Memory {
   explicit Memory(int val) : x(val) {}
 };
 
+class Entity {
+  public:
+  Entity() {
+    cout << "Entity created\n";
+  }
+  void print() {
+    cout << "Print Called\n";
+  }
+  ~Entity() {
+    cout << "Entity deleted\n";
+  }
+};
+
 int main() {
-  auto* ptr = new Memory(3);
-  cout << ptr->x;
+  shared_ptr<Entity> e1;
+  {
+    unique_ptr<Entity> unq_ent = make_unique<Entity>();
+    unq_ent->print();
+    cout << "\n";
+    shared_ptr<Entity> shared_entity = make_shared<Entity>();
+    // unique_ptr<Entity> e0 = ent;
+    e1 = shared_entity;
+    // unq_ent will be deleted and shared_ptr's references will be reduced to 1
+  }
+  cout << "\nOut of nested scope\n";
+  e1->print();
+  Memory* ptr = new Memory(3);
+  cout << ptr->x << "\n";
   delete ptr;
   return 0;
+  // Finally shared_ptr will be deleted
 }
