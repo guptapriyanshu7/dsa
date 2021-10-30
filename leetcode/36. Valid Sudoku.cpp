292453de193236652931f75f01459ccd575e2563
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-10-30 19:27:34 +0530

    commit

---
+++ leetcode/36. Valid Sudoku.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool solve(vector<vector<char>>& board) {
+  return true;
+}
+
+int main() {
+  vector<vector<char>> nums{};
+  solve(nums);
+  return 0;
+}
