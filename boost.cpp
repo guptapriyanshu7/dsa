73061c2c31424e34c3105708271451183f393b0c
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2021-11-05 00:24:51 +0530

    using boost
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- boost.cpp
+++ boost.cpp
@@ -1,7 +1,8 @@
-// #include <boost/lambda/lambda.hpp>
+// #include <algorithm>
 // #include <iostream>
 // #include <iterator>
-// #include <algorithm>
+
+// #include <boost/lambda/lambda.hpp>
 
 // int main() {
 //   using namespace boost::lambda;
@@ -9,13 +10,13 @@
 
 //   std::for_each(
 //     in(std::cin), in(),
-//     std::cout << (_1 * 3) << " "
-//   );
+//     std::cout << (_1 * 3) << " ");
 // }
 
-// #include <boost/regex.hpp>
 // #include <iostream>
 // #include <string>
+
+// #include <boost/regex.hpp>
 
 // int main() {
 //   std::string line;
@@ -29,31 +30,35 @@
 //   }
 // }
 
-#include<iostream>
-#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <iostream>
+
 #include <boost/math/constants/constants.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
 using boost::multiprecision::cpp_dec_float_50;
 using namespace std;
+
 template<typename T>
 inline T circle_area(T r) {
   // pi is predefined constant having value
   using boost::math::constants::pi;
   return pi<T>() * r * r;
 }
+
 int main() {
-  float f_rad = 243.0 / 100;
-  float f_area = circle_area(f_rad);
-  double d_rad = 243.0 / 100;
-  double d_area = circle_area(d_rad);
-  cpp_dec_float_50 rad_mp = 243.0 / 100;
+  float f_rad              = 243.0 / 100;
+  float f_area             = circle_area(f_rad);
+  double d_rad             = 243.0 / 100;
+  double d_area            = circle_area(d_rad);
+  cpp_dec_float_50 rad_mp  = 243.0 / 100;
   cpp_dec_float_50 area_mp = circle_area(rad_mp);
-  cout << "Float: " << setprecision(numeric_limits<float>::digits10) << f_area <<
-    endl;
+
+  cout << "Float: " << setprecision(numeric_limits<float>::digits10) << f_area << endl;
+
   // Double area
-  cout << "Double: " << setprecision(numeric_limits<double>::digits10) << d_area
-    << endl;
+  cout << "Double: " << setprecision(numeric_limits<double>::digits10) << d_area << endl;
+
   // Area by using Boost Multiprecision
-  cout << "Boost Multiprecision Res: " <<
-    setprecision(numeric_limits<cpp_dec_float_50>::digits10) << area_mp << endl;
+  cout << "Boost Multiprecision Res: " << setprecision(numeric_limits<cpp_dec_float_50>::digits10) << area_mp << endl;
   return 0;
 }
